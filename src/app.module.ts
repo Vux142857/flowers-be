1b4ed65d8ea0d9111b3575494ee9f92ae3490435
Vu142857 <thanhvu6a1@gmail.com>
2024-08-16 00:04:02 +0700

    feat: WIP-Users-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
