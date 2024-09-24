0f5be93f818f56257a03265ba7ab44832d4d6d1e
ttvu <thanhvu6a1@gmail.com>
2024-09-24 10:18:46 +0700

    feat: Create order func

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,12 @@ import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Order from './entities/order.entity';
 import { OrderItem } from './entities/order-items.entity';
+import { OrderService } from './providers/order.service';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
   controllers: [OrdersController],
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem]), ProductsModule],
+  providers: [OrderService],
 })
 export class OrdersModule {}
---
+++ src/orders/providers/order.service.ts
@@ -0,0 +1,93 @@
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import Order from '../entities/order.entity';
+import { OrderItem } from '../entities/order-items.entity';
+import { ProductService } from 'src/products/providers/product.service';
+import { CreateOrderDto } from '../dtos/create-order.dto';
+
+@Injectable()
+export class OrderService {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+
+    @InjectRepository(OrderItem)
+    private readonly orderItemRepository: Repository<OrderItem>,
+
+    private readonly productService: ProductService,
+  ) {}
+
+  async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
+    const queryRunner =
+      this.orderRepository.manager.connection.createQueryRunner();
+    await queryRunner.startTransaction();
+
+    try {
+      const { customer, total, orderItems } = createOrderDto;
+
+      // Create Order entity
+      const order = this.orderRepository.create({
+        customer,
+        total,
+        statusOrder: createOrderDto.statusOrder,
+      });
+
+      // Process OrderItem
+      const processedOrderItems: OrderItem[] = [];
+
+      for (const itemDto of orderItems) {
+        const product = await this.productService.getProductById(
+          itemDto.product.id,
+        );
+
+        if (!product) {
+          throw new NotFoundException(
+            `Product with ID ${itemDto.product.id} not found`,
+          );
+        }
+
+        // Check stock before save OrderItem
+        const isAvailable = await this.productService.checkStock(
+          product.id,
+          itemDto.quantity,
+        );
+        if (!isAvailable) {
+          throw new ConflictException(
+            `Product ${product.name} is out of stock`,
+          );
+        }
+
+        // Save OrderItem
+        const orderItem = this.orderItemRepository.create({
+          product,
+          quantity: itemDto.quantity,
+          subTotal: itemDto.subTotal,
+          order,
+        });
+
+        // Derease stock
+        await this.productService.decreaseStock(product.id, itemDto.quantity);
+
+        processedOrderItems.push(orderItem);
+      }
+
+      // Save Order and OrderItems
+      order.orderItems = processedOrderItems;
+      await queryRunner.manager.save(Order, order);
+      await queryRunner.manager.save(OrderItem, processedOrderItems);
+
+      await queryRunner.commitTransaction();
+      return order;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
